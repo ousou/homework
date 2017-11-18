37d7a2e00fdcec1929ec426dfd1967df5ff7e26b
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-11-18 10:09:11 +0200

    HW4 - Implemented RandomController

--- hw4/controllers.py
+++ hw4/controllers.py
@@ -13,14 +13,11 @@ class Controller():
 
 class RandomController(Controller):
 	def __init__(self, env):
-		""" YOUR CODE HERE """
-		pass
+		self.env = env
 
 	def get_action(self, state):
 		""" YOUR CODE HERE """
-		""" Your code should randomly sample an action uniformly from the action space """
-		pass
-
+		return self.env.action_space.sample()
 
 class MPCcontroller(Controller):
 	""" Controller built using the MPC method outlined in https://arxiv.org/abs/1708.02596 """
