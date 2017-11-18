de249f2f038b2f4e3639cc2c498668a5a42844e7
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-11-18 11:24:17 +0200

    HW4 - First version of NNDynamicsModel predict function. No fit function yet

--- hw4/dynamics.py
+++ hw4/dynamics.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import gym
 
 # Predefined function to build a feedforward neural network
 def build_mlp(input_placeholder, 
@@ -30,8 +31,22 @@ class NNDynamicsModel():
                  learning_rate,
                  sess
                  ):
-        """ YOUR CODE HERE """
-        """ Note: Be careful about normalization """
+        self.env = env
+        self.state_mean = normalization[0]
+        self.state_std = normalization[1]
+        self.delta_mean = normalization[2]
+        self.delta_std = normalization[3]
+        self.act_mean = normalization[4]
+        self.act_std = normalization[5]
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self.sess = sess
+        ob_dim = env.observation_space.shape[0]
+        ac_dim = env.action_space.n if isinstance(env.action_space, gym.spaces.Discrete) else env.action_space.shape[0]
+        self.input_ph = tf.placeholder(shape=[None, ob_dim + ac_dim], name="input", dtype=tf.float32)
+        self.model = build_mlp(self.input_ph, ob_dim, "dyn_model", n_layers, size, activation, output_activation)
+        self.eps = 0.000001
 
     def fit(self, data):
         """
@@ -41,5 +56,24 @@ class NNDynamicsModel():
         """YOUR CODE HERE """
 
     def predict(self, states, actions):
-        """ Write a function to take in a batch of (unnormalized) states and (unnormalized) actions and return the (unnormalized) next states as predicted by using the model """
-        """ YOUR CODE HERE """
+        output_states = []
+        for i in range(len(states)):
+            state_input = self._normalize_state(states[i])
+            action_input = self._normalize_action(actions[i])
+            # state_input = (states[i] - self.state_mean) / (self.state_std + self.eps)
+            # action_input = (actions[i] - self.act_mean) / (self.act_std + self.eps)
+            input = np.concatenate((state_input, action_input), axis=0)
+            state_delta = self.sess.run(self.model, feed_dict={self.input_ph: [input]})
+            state_delta = self._denormalize_state_diff(state_delta)
+            output_states.append(np.sum(state_delta, states[i]))
+        return np.array(output_states)
+
+    def _denormalize_state_diff(self, state):
+        state = np.array(state)
+        return self.delta_mean + (self.delta_std * state)
+
+    def _normalize_state(self, state):
+        return (state - self.state_mean) / (self.state_std + self.eps)
+
+    def _normalize_action(self, action):
+        return (action - self.act_mean) / (self.act_std + self.eps)
